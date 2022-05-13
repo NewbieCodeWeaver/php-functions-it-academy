38f4e8112cb45b8aa8211ee46ce3df8ef7270833
Newbiecodeweaver <carlosgilino@gmail.com>
2022-05-13 12:31:42 +0200

    Entrega del primer ejercicio del tema 3

---
+++ Nivel_1/Ex1_Nivel1.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$edad = 38;
+
+saberParImpar($edad);
+
+
+function saberParImpar($a) {
+
+if ($a % 2 == 0) {
+
+echo "El número es par";
+} else {
+
+    echo "El número es impar";
+}
+
+
+}
+
+
+?>
